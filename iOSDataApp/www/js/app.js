1cd7e69773cfd36829e5f52a169d1c9ba8927c2d
Nignesh <NiGi@Nigneshs-MacBook-Pro.local>
2015-04-07 05:06:35 -0400

    implement HTML navigation flow

--- iOSDataApp/www/js/app.js
+++ iOSDataApp/www/js/app.js
@@ -1,27 +1,72 @@
-//app.js
-
-var app= {
-	loadRequirements:0,
-	init: function(){
+var app = {
+	loadRequirements: 0,
+	init: function () {
 		document.addEventListener("deviceready", app.onDeviceReady);
 		document.addEventListener("DOMContentLoaded", app.onDomReady);
 	},
-	onDeviceReady: function(){
+	onDeviceReady: function () {
 		app.loadRequirements++;
-		if(app.loadRequirements === 2){
+		if (app.loadRequirements === 2) {
 			app.start();
 		}
 	},
-	onDomReady: function(){
+	onDomReady: function () {
 		app.loadRequirements++;
-		if(app.loadRequirements === 2){
-			app.start();
-		}
+		//		if (app.loadRequirements === 2) {
+		//			app.start();
+		//		}
+
+		app.start();
 	},
-	start: function(){
+	start: function () {
 		//connect to database
 		//build the lists for the main pages based on data
 		//add button and navigation listeners
+
+		var section1 = document.getElementById("people-list");
+		var section2 = document.getElementById("occasion-list");
+
+		Hammer(section1).on("swipeleft", function () {
+			//			alert("swipeleft");
+			$(section1).animate({
+				left: "-100%"
+			}, 200);
+			$(section2).animate({
+				left: "0"
+			}, 200);
+		});
+
+		Hammer(section2).on("swiperight", function () {
+			//			alert("swiperight");
+			$(section1).animate({
+				left: "0"
+			}, 200);
+			$(section2).animate({
+				left: "100%"
+			}, 200);
+		});
+
+		var elem = document.getElementById('mySwipe');
+		window.mySwipe = Swipe(elem, {
+			// startSlide: 4, 
+			// auto: 3000, 
+			// continuous: true, 
+			// disableScroll: true, 
+			// stopPropagation: true, 
+			// callback: function(index, element) {}, 
+			// transitionEnd: function(index, element) {} 
+		});
+		
+		var elem = document.getElementById('mySwipe1');
+		window.mySwipe1 = Swipe(elem, {
+			// startSlide: 4, 
+			// auto: 3000, 
+			// continuous: true, 
+			// disableScroll: true, 
+			// stopPropagation: true, 
+			// callback: function(index, element) {}, 
+			// transitionEnd: function(index, element) {} 
+		});
 	}
 }
 
